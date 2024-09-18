831b58d151da0a44dbe38fb97511295ac98d6220
Guilherme Machado <guilhermemach777@gmail.com>
2024-09-17 23:47:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const nota = 90;
+
+switch(nota) {
+    case 90:
+        console.log("Sua nota é A")
+        break;
+    case 80:
+        console.log("Sua nota é B")
+        break;
+    case 70:
+        console.log("Sua nota é C")
+        break;
+    default:
+        console.log("Sua nota é D")
+
+}
+
+ /*    if (nota >= 90) {
+        console.log("Sua nota é A")
+    } else if (nota >= 80) {
+        console.log("Sua nota é B")
+    } else if (nota >= 70) {
+        console.log("Sua nota é C")
+    } else if (nota >= 60) {
+        console.log("Sua nota é D")
+    } else {
+        console.log("Sua nota é E")
+    }
+ */
+
+
+
+
